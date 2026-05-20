2e53a2f5ffbab88f9d90469bb8bcd8ee36050626
debbie-huang-appier <debbie.huang@appier.com>
2026-05-20 16:18:55 +0800

    Remove client_secret from WorkspaceClient
    Remove sensitive client_secret from WorkspaceClient initialization.

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from databricks_mcp import DatabricksMCPClient
 ws = WorkspaceClient(
     host="https://1005657065394160.0.gcp.databricks.com",
     client_id="32b93186-4dd1-47ba-811f-ca11f9d77f45",
-    client_secret="dose40fb98ab446bda2a0526592b7980d17a",
+    client_secret="",
 )
 
 mcp = DatabricksMCPClient(
